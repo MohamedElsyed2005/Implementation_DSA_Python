fe917e950b37bdfb59efa8c533130b9ace9762be
mohamed <2305273@anu.edu.eg>
2024-12-14 21:31:47 +0200

    edit BlncdPar.py

--- Data_structure/Linear_list/BlncdPar.py
+++ Data_structure/Linear_list/BlncdPar.py
@@ -8,11 +8,11 @@ Valid input characters: (, ), {, }, [, ]
 def isValid(s):
     size = len(s)
     stack = Stack(size)  # Initialize stack with given size
-
+    
     for char in s:
         if char in {')', ']', '}'}:
             if stack.isEmpty():
-                return False  # Unbalanced if stack is empty
+                return "Not Balanced"  # Unbalanced if stack is empty
             if char == '}' and stack.get_top() == '{':
                 stack.pop()
             elif char == ']' and stack.get_top() == '[':
@@ -20,16 +20,15 @@ def isValid(s):
             elif char == ')' and stack.get_top() == '(':
                 stack.pop()
             else:
-                return False  # Mismatch case
-        else:
+                return "Not Balanced"  # Mismatch case
+        elif char in {'(', '[', '{'}:
             stack.push(char)
+        else:
+            return "illegal char in input"
     
     stack.Display()
-    return stack.isEmpty()  # Balanced if stack is empty
+    return "properly Nested stucture"  # Balanced if stack is empty
 
 if __name__ == '__main__':
     s = input("Enter your text: ")
-    if isValid(s):
-        print("Balanced")
-    else:
-        print("Not Balanced")
+    print(isValid(s))
